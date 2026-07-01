d819dc44b28979fe1fdf00cab566011e70aeceef
Mohammed Harish <mohamharish0007@gmail.com>
2026-07-01 16:45:32 +0530

    added basic java programs

---
+++ BinarySearch.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72, 91};
+        int target = 23;
+        int result = binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
