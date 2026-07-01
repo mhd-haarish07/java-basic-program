bc16b4a20a5f5fad692404173567adca8e01b7c4
Mohammed Harish <mohamharish0007@gmail.com>
2026-07-01 17:14:59 +0530

    added basic java programs

---
+++ TransposeMatrix.java
@@ -0,0 +1,33 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        int[][] transposed = transpose(matrix);
+        System.out.println("Transposed Matrix:");
+        printMatrix(transposed);
+    }
+
+    static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int[][] result = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+}
