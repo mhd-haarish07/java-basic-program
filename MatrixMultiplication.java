8c620129dbbb248bae1b05284bd5cd58299509aa
Mohammed Harish <mohamharish0007@gmail.com>
2026-07-01 16:46:34 +0530

    added basic java programs

---
+++ MatrixMultiplication.java
@@ -0,0 +1,33 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2}, {3, 4}};
+        int[][] b = {{5, 6}, {7, 8}};
+
+        int[][] result = multiplyMatrices(a, b);
+
+        System.out.println("Resultant Matrix:");
+        printMatrix(result);
+    }
+
+    static int[][] multiplyMatrices(int[][] a, int[][] b) {
+        int rowsA = a.length, colsA = a[0].length, colsB = b[0].length;
+        int[][] result = new int[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
